6552b20bfaa36f53b7dcf0a4af46bbe3dddab0cb
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-08-13 10:20:24 +0200

    refactor: delete GitHubQueryCriteria, GitHubQueryResult classes and replace byCriteria function

--- src/main/kotlin/provider/GitHubSearchQuery.kt
+++ src/main/kotlin/provider/GitHubSearchQuery.kt
@@ -7,46 +7,16 @@ import java.net.URL
 /**
  * A class implementing a search query for GitHub repositories.
  */
-class GitHubSearchQuery : RepositorySearchQuery {
+class GitHubSearchQuery : RepositorySearchQuery<GHRepositorySearchBuilder, GitHubSearchCriteria> {
 
     private val github: GitHub = GitHub.connectAnonymously()
 
-    override fun byLink(url: URL): RepositorySearchQuery.RepositoryQueryResult {
+    override fun byLink(url: URL): Iterable<Repository> {
         val repositoryPath = url.path.substringAfter("/")
-        return GitHubQueryResult(setOf(GitHubRepository(github.getRepository(repositoryPath))))
+        return setOf(GitHubRepository(github.getRepository(repositoryPath)))
     }
 
-    override fun byCriteria(): RepositorySearchQuery.RepositoryQueryCriteria = GitHubQueryCriteria()
-
-    /**
-     * A class implementing GitHub search query criteria.
-     */
-    private inner class GitHubQueryCriteria : RepositorySearchQuery.RepositoryQueryCriteria {
-
-        private val searchCriteria: GHRepositorySearchBuilder = this@GitHubSearchQuery.github.searchRepositories()
-
-        override fun byName(repositoryName: String): RepositorySearchQuery.RepositoryQueryCriteria {
-            searchCriteria.q(repositoryName)
-            return this
-        }
-
-        override fun byUser(user: String): RepositorySearchQuery.RepositoryQueryCriteria {
-            searchCriteria.user(user)
-            return this
-        }
-
-        override fun search(): RepositorySearchQuery.RepositoryQueryResult {
-            return GitHubQueryResult(
-                searchCriteria.list()
-                    .map { GitHubRepository(it) }
-            )
-        }
+    override fun byCriteria(criteria: GitHubSearchCriteria): Iterable<Repository> {
+        return criteria.apply().list().map { GitHubRepository(it) }
     }
-
-    /**
-     * A github query result.
-     */
-    private class GitHubQueryResult(
-        override val result: Iterable<Repository>
-    ) : RepositorySearchQuery.RepositoryQueryResult
 }
