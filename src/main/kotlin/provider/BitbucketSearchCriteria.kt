4e4b567c5d3c2a9c7405aecbe5511b7059994e7b
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-08-13 10:07:35 +0200

    feat: add bitbucket search criteria

---
+++ src/main/kotlin/provider/BitbucketSearchCriteria.kt
@@ -0,0 +1,39 @@
+package provider
+
+private const val BASE_URL: String = "https://api.bitbucket.org/2.0/repositories/"
+
+/**
+ * An interface modeling search criteria for searching Bitbucket repositories.
+ */
+interface BitbucketSearchCriteria : SearchCriteria<String>
+
+/**
+ * A criteria matching by username.
+ */
+class ByBitbucketUser(private val username: String) : BitbucketSearchCriteria {
+    override fun apply(): String = BASE_URL.plus("$username?")
+}
+
+/**
+ * A decorator of [BitbucketSearchCriteria] for compound criteria.
+ */
+abstract class BitbucketCompoundCriteria(
+    private val criteria: BitbucketSearchCriteria
+) : BitbucketSearchCriteria {
+    override fun apply(): String = criteria.apply()
+}
+
+/**
+ * A criteria matching by repository name.
+ */
+class ByBitbucketName(
+    private val repositoryName: String,
+    criteria: BitbucketSearchCriteria
+) : BitbucketCompoundCriteria(criteria) {
+    override fun apply(): String {
+        var url = super.apply()
+        url += if (url.endsWith("?")) "q=" else "+AND+"
+        url += "name+%7E+%22$repositoryName%22"
+        return url
+    }
+}
