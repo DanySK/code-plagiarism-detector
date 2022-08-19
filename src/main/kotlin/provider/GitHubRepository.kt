3104230d7bcf3b2cd16aa0336c49847ee8915f5e
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-08-19 16:44:23 +0200

    refactor: add source filtering

--- src/main/kotlin/provider/GitHubRepository.kt
+++ src/main/kotlin/provider/GitHubRepository.kt
@@ -9,21 +9,23 @@ private const val RECURSION = 1
  * A GitHub repository adapter.
  * @property repository the [GHRepository] to be adapted
  */
-data class GitHubRepository(private val repository: GHRepository) : Repository {
+data class GitHubRepository(private val repository: GHRepository) : AbstractRepository() {
     override val name: String
         get() = repository.name
 
     override val contributors: Iterable<String>
         get() = repository.listContributors().map { it.name }
 
-    override val sources: Iterable<InputStream>
-        get() {
-            val defaultBranch = repository.defaultBranch
-                ?: error("The repository has not yet appropriately configured: no default branch configured.")
-            return repository.getTreeRecursive(defaultBranch, RECURSION).tree
-                .asSequence()
-                .filter { it.type == "blob" }
-                .map { it.readAsBlob() }
-                .toSet()
-        }
+    override fun extractSources(languageExtensions: Iterable<String>): Iterable<InputStream> {
+        val defaultBranch = repository.defaultBranch
+            ?: error("The repository has not yet appropriately configured: no default branch configured.")
+        return repository.getTreeRecursive(defaultBranch, RECURSION).tree
+            .asSequence()
+            .filter { it.type == "blob" && extensionFilter(it.path, languageExtensions) }
+            .map { it.readAsBlob() }
+            .toSet()
+    }
+
+    private fun extensionFilter(path: String, extensions: Iterable<String>) =
+        extensions.any { path.endsWith(it) }
 }
