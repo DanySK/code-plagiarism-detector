9775fbe3c06dd373949671f977ed14e6221f563d
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-08-19 21:54:48 +0200

    refactor: change github api library

--- src/main/kotlin/provider/GitHubRepository.kt
+++ src/main/kotlin/provider/GitHubRepository.kt
@@ -1,31 +1,20 @@
 package provider
 
-import org.kohsuke.github.GHRepository
-import java.io.InputStream
-
-private const val RECURSION = 1
+import com.jcabi.github.Repo
 
 /**
  * A GitHub repository adapter.
- * @property repository the [GHRepository] to be adapted
+ * @property repository the [Repo] to be adapted
  */
-data class GitHubRepository(private val repository: GHRepository) : AbstractRepository() {
+data class GitHubRepository(private val repository: Repo) : AbstractRepository() {
+
     override val name: String
-        get() = repository.name
+        get() = repository.coordinates().repo()
 
     override val contributors: Iterable<String>
-        get() = repository.listContributors().map { it.name }
+        get() = repository.collaborators().iterate().map { it.login() }
 
-    override fun extractSources(languageExtensions: Iterable<String>): Iterable<InputStream> {
-        val defaultBranch = repository.defaultBranch
-            ?: error("The repository has not yet appropriately configured: no default branch configured.")
-        return repository.getTreeRecursive(defaultBranch, RECURSION).tree
-            .asSequence()
-            .filter { it.type == "blob" && extensionFilter(it.path, languageExtensions) }
-            .map { it.readAsBlob() }
-            .toSet()
+    override fun getCloneUrl(): String {
+        return "https://github.com/${repository.coordinates().user()}/${repository.coordinates().repo()}"
     }
-
-    private fun extensionFilter(path: String, extensions: Iterable<String>) =
-        extensions.any { path.endsWith(it) }
 }
