64964c369306dcae01151820f0fdb8b84bca7e31
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-08-19 16:47:13 +0200

    refactor: error handling

--- src/main/kotlin/provider/GitHubSearchQuery.kt
+++ src/main/kotlin/provider/GitHubSearchQuery.kt
@@ -1,22 +1,44 @@
 package provider
 
+import org.kohsuke.github.GHException
 import org.kohsuke.github.GHRepositorySearchBuilder
 import org.kohsuke.github.GitHub
+import org.slf4j.LoggerFactory
+import java.io.FileNotFoundException
 import java.net.URL
 
 /**
  * A class implementing a search query for GitHub repositories.
  */
 class GitHubSearchQuery : RepositorySearchQuery<GHRepositorySearchBuilder, GitHubSearchCriteria> {
+    private val logger = LoggerFactory.getLogger(this.javaClass.name)
 
+    // TODO authentication
     private val github: GitHub = GitHub.connectAnonymously()
 
     override fun byLink(url: URL): Iterable<Repository> {
-        val repositoryPath = url.path.substringAfter("/")
-        return setOf(GitHubRepository(github.getRepository(repositoryPath)))
+        try {
+            return getMatchingReposByPath(url.path.substringAfter("/"))
+        } catch (e: FileNotFoundException) {
+            logger.error("No matching repos at url $url: ${e.message}")
+        }
+        return emptySet()
+    }
+
+    private fun getMatchingReposByPath(path: String): Iterable<GitHubRepository> {
+        return setOf(GitHubRepository(github.getRepository(path)))
     }
 
     override fun byCriteria(criteria: GitHubSearchCriteria): Iterable<Repository> {
+        try {
+            return getMatchingReposByCriteria(criteria)
+        } catch (e: GHException) {
+            logger.error("No user: ${e.message}")
+        }
+        return emptySet()
+    }
+
+    private fun getMatchingReposByCriteria(criteria: GitHubSearchCriteria): Iterable<GitHubRepository> {
         return criteria.apply().list().map { GitHubRepository(it) }
     }
 }
