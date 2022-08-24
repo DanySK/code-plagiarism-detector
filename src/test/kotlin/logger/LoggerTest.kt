03d5e29c28866464fe99b856c43020669ee8b254
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-08-24 09:21:50 +0200

    test: add logger test, remove not useful afterSpec block

---
+++ src/test/kotlin/logger/LoggerTest.kt
@@ -0,0 +1,31 @@
+package logger
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.read.ListAppender
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldNotBeEmpty
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldMatch
+import org.slf4j.LoggerFactory
+
+private const val LOGGING_MSG = "This is a sample logging message"
+
+class LoggerTest : FunSpec({
+    val logger = LoggerFactory.getLogger(this.javaClass) as Logger
+    val listAppender = ListAppender<ILoggingEvent>()
+    listAppender.start()
+    logger.addAppender(listAppender)
+
+    beforeSpec {
+        logger.debug(LOGGING_MSG)
+    }
+
+    test("Messages should contain the logged message with the correct level") {
+        val logs = listAppender.list
+        logs.shouldNotBeEmpty()
+        logs.map { it.message }.first().shouldMatch(LOGGING_MSG)
+        logs.map { it.level }.first().shouldBe(Level.DEBUG)
+    }
+})
