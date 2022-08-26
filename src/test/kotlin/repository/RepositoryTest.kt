f00e2dfe6a3b38ed372933f1ec943e84c84a0e34
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-08-26 09:52:42 +0200

    test: replace repos which may change with ones dedicated to tests

--- src/test/kotlin/repository/RepositoryTest.kt
+++ src/test/kotlin/repository/RepositoryTest.kt
@@ -7,18 +7,17 @@ import provider.BitbucketProvider
 import provider.GitHubProvider
 import java.net.URL
 
-private const val GH_SAMPLE_REPO_URL = "https://github.com/APICe-at-DISI/sample-javafx-project"
-private const val BB_SAMPLE_REPO_URL = "https://bitbucket.org/danysk/courses-oop-gradle-jfx-template"
-private const val EXPECTED_GH_REPO_SOURCES = 9
-private const val EXPECTED_BB_REPO_SOURCES = 3
+private const val GH_SAMPLE_REPO_URL = "https://github.com/tassiLuca/test-app-for-code-plagiarism-detector"
+private const val BB_SAMPLE_REPO_URL = "https://bitbucket.org/tassiLuca/test-app-for-code-plagiarism-detector"
+private const val EXPECTED_SOURCES = 8
 
 class RepositoryTest : FunSpec({
-    val githubRepo = GitHubProvider().byLink(URL(GH_SAMPLE_REPO_URL))!!
-    val bitbucketRepo = BitbucketProvider().byLink(URL(BB_SAMPLE_REPO_URL))!!
+    val githubRepo = GitHubProvider().byLink(URL(GH_SAMPLE_REPO_URL))
+    val bitbucketRepo = BitbucketProvider().byLink(URL(BB_SAMPLE_REPO_URL))
 
     test("Check if returns all sources") {
-        githubRepo.getSources("java").count() shouldBeExactly EXPECTED_GH_REPO_SOURCES
-        bitbucketRepo.getSources("java").count() shouldBeExactly EXPECTED_BB_REPO_SOURCES
+        githubRepo.getSources("java").count() shouldBeExactly EXPECTED_SOURCES
+        bitbucketRepo.getSources("java").count() shouldBeExactly EXPECTED_SOURCES
     }
 
     test("Trying to get sources of unknown language should throw an exception") {
