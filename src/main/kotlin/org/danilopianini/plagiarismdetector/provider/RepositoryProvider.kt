881825cf0df36fd2f650948749e9711d764a2e5a
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-08-26 14:11:53 +0200

    refactor: replace toMutableList with toList

--- src/main/kotlin/org/danilopianini/plagiarismdetector/provider/RepositoryProvider.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/provider/RepositoryProvider.kt
@@ -13,7 +13,7 @@ interface RepositoryProvider<T, in C : SearchCriteria<T>> {
     /**
      * Search a repository by the given [URL].
      * @param url the [URL] address in `owner/name` format of the public repository to retrieve.
-     * @return the requested [Repository] if exists, or null if not.
+     * @return the requested [Repository] if exists.
      * @throws IllegalArgumentException if the [url] doesn't match the `owner/name` pattern or is not correct.
      */
     fun byLink(url: URL): Repository
