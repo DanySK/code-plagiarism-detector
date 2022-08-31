af7747f74eb358a996336b9838bf9d5ff1a8a67f
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-08-31 19:06:26 +0200

    test: added mock providers

--- src/test/kotlin/org/danilopianini/plagiarismdetector/provider/ProviderTest.kt
+++ src/test/kotlin/org/danilopianini/plagiarismdetector/provider/ProviderTest.kt
@@ -1,5 +1,6 @@
 package org.danilopianini.plagiarismdetector.provider
 
+import com.mashape.unirest.http.Unirest
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldBeEmpty
@@ -8,13 +9,25 @@ import io.kotest.matchers.collections.shouldNotContainDuplicates
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.string.shouldContainIgnoringCase
 import io.kotest.matchers.string.shouldMatch
+import io.mockk.every
+import io.mockk.slot
+import io.mockk.spyk
+import org.apache.commons.io.IOUtils
 import org.danilopianini.plagiarismdetector.provider.criteria.ByBitbucketName
 import org.danilopianini.plagiarismdetector.provider.criteria.ByBitbucketUser
 import org.danilopianini.plagiarismdetector.provider.criteria.ByGitHubName
 import org.danilopianini.plagiarismdetector.provider.criteria.ByGitHubUser
+import org.danilopianini.plagiarismdetector.provider.criteria.GitHubSearchCriteria
+import org.danilopianini.plagiarismdetector.repository.GitHubRepository
 import org.danilopianini.plagiarismdetector.repository.Repository
+import org.json.JSONObject
+import org.kohsuke.github.GHFileNotFoundException
+import org.kohsuke.github.GitHub
+import org.slf4j.LoggerFactory
 import java.net.URL
+import java.nio.charset.StandardCharsets
 
+private const val PR_BUILD_VARIABLE = "PR_BUILD"
 private const val GH_URL_PREFIX = "https://github.com"
 private const val BB_URL_PREFIX = "https://bitbucket.org"
 private const val DANYSK_USERNAME = "Danilo Pianini"
@@ -24,9 +37,18 @@ private const val TASSILUCA_USER = "tassiLuca"
 private const val TASSILUCA_USERNAME = "Luca Tassinari"
 
 class ProviderTest : FunSpec() {
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+    private val providerTestsEnabled = System.getenv(PR_BUILD_VARIABLE) != "true"
+    private var githubProvider = GitHubProvider()
+    private var bitbucketProvider = BitbucketProvider()
+
     init {
-        val githubProvider = GitHubProvider()
-        val bitbucketProvider = BitbucketProvider()
+        beforeSpec {
+            if (!providerTestsEnabled) {
+                createMockBitbucketProvider()
+                createMockGitHubProvider()
+            }
+        }
 
         test("Searching by existing name and user should return repos matching those criteria") {
             var searchedRepoName = "Project-OOP"
@@ -73,13 +95,17 @@ class ProviderTest : FunSpec() {
                 githubProvider.byLink(URL("https://www.unibo.it/"))
             }
             shouldThrow<java.lang.IllegalArgumentException> {
-                githubProvider.byLink(URL("$GH_URL_PREFIX/$DANYSK_GH_USER/code-plagiarism-detector/tree/master/src/"))
+                githubProvider.byLink(
+                    URL("$GH_URL_PREFIX/$DANYSK_GH_USER/code-plagiarism-detector/tree/master/src/")
+                )
             }
             shouldThrow<java.lang.IllegalArgumentException> {
                 bitbucketProvider.byLink(URL("https://www.unibo.it/"))
             }
             shouldThrow<java.lang.IllegalArgumentException> {
-                bitbucketProvider.byLink(URL("$BB_URL_PREFIX/$DANYSK_BB_USER/courses-oop-gradle-jfx-template/src/"))
+                bitbucketProvider.byLink(
+                    URL("$BB_URL_PREFIX/$DANYSK_BB_USER/courses-oop-gradle-jfx-template/src/")
+                )
             }
         }
 
@@ -115,5 +141,45 @@ class ProviderTest : FunSpec() {
         result.shouldNotBeNull()
         result.name shouldMatch expectedName
         result.owner shouldMatch expectedUser
+    }
+
+    private fun createMockBitbucketProvider() {
+        logger.info(
+            "Testing bitbucket provider with anonymously connection: rate limits exists " +
+                "(see https://support.atlassian.com/bitbucket-cloud/docs/api-request-limits/)"
+        )
+        val urlSlot = slot<String>()
+        bitbucketProvider = spyk(BitbucketProvider(), recordPrivateCalls = true)
+        every { bitbucketProvider invoke "doGETRequest" withArguments listOf(capture(urlSlot)) } answers {
+            val response = Unirest.get(urlSlot.captured)
+                .header("Accept", "application/json")
+                .asBinary()
+            JSONObject(IOUtils.toString(response.body, StandardCharsets.UTF_8))
+        }
+    }
+
+    private fun createMockGitHubProvider() {
+        logger.info(
+            "Testing github provider with anonymously connection: rate limits exists " +
+                "(see https://docs.github.com/en/rest/rate-limit)"
+        )
+        val criteriaSlot = slot<GitHubSearchCriteria>()
+        githubProvider = spyk(GitHubProvider(), recordPrivateCalls = true)
+        every {
+            githubProvider invoke "getMatchingReposByCriteria" withArguments listOf(capture(criteriaSlot))
+        } answers {
+            criteriaSlot.captured.apply(GitHub.connectAnonymously()).list().toSet().asSequence()
+                .map { GitHubRepository(it) }
+                .toSet()
+        }
+        val urlSlot = slot<URL>()
+        every { githubProvider invoke "getRepoByUrl" withArguments listOf(capture(urlSlot)) } answers {
+            val repoName = urlSlot.captured.path.replace(Regex("^/|/$"), "")
+            try {
+                GitHubRepository(GitHub.connectAnonymously().getRepository(repoName))
+            } catch (e: GHFileNotFoundException) {
+                throw IllegalArgumentException("FILE NOT FOUND: $e")
+            }
+        }
     }
 }
