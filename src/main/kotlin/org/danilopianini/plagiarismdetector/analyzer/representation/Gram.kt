65a67c837c94726bc998989efc113d36d7a4f412
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-13 15:00:43 +0200

    feat(analyzer): add source representation interfaces

---
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/representation/Gram.kt
@@ -0,0 +1,13 @@
+package org.danilopianini.plagiarismdetector.analyzer.representation
+
+/**
+ * An interface modeling a [n-gram](https://en.wikipedia.org/wiki/N-gram),
+ * a contiguous sequence of items.
+ * @param T the type of the items that is composed of.
+ */
+interface Gram<T> {
+    /**
+     * The items that compose the gram.
+     */
+    val items: Sequence<T>
+}
---
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/representation/SourceRepresentation.kt
@@ -0,0 +1,20 @@
+package org.danilopianini.plagiarismdetector.analyzer.representation
+
+import java.io.File
+
+/**
+ * An interface modeling the intermediate representation, which
+ * is generated from the source file, prior to comparison.
+ * @param <T> the type of the representation.
+ */
+interface SourceRepresentation<T> {
+    /**
+     * The source file of this representation.
+     */
+    val sourceFile: File
+
+    /**
+     * The representation of the source file.
+     */
+    val representation: T
+}
