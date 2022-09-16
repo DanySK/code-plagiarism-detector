0714f66e85c8cc21e1bc22256c5b36dfe6156d93
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-16 16:04:09 +0200

    feat(token): add LanguageTokenTypes

---
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/representation/token/languages/JavaTokenTypes.kt
@@ -0,0 +1,16 @@
+package org.danilopianini.plagiarismdetector.analyzer.representation.token.languages
+
+import org.danilopianini.plagiarismdetector.analyzer.representation.token.TokenType
+
+/**
+ * A simple class implementing a [LanguageTokenTypes] for Java programming language.
+ */
+data class JavaTokenTypes(private val tokensTypes: Sequence<TokenType>) : LanguageTokenTypes {
+    override fun getTokenTypeBy(constructName: String): TokenType {
+        require(isValidToken(constructName))
+        return tokensTypes.find { it.languageConstructs.contains(constructName) }!!
+    }
+
+    override fun isValidToken(constructName: String): Boolean =
+        tokensTypes.any { it.languageConstructs.contains(constructName) }
+}
