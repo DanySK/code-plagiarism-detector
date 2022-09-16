c7283c026cba151e8c43b6259dbd516153eb28f9
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-16 16:31:33 +0200

    feat(analyzer): add analyzer

---
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/TokenizationAnalyzer.kt
@@ -0,0 +1,25 @@
+package org.danilopianini.plagiarismdetector.analyzer
+
+import org.danilopianini.plagiarismdetector.analyzer.representation.token.Token
+import org.danilopianini.plagiarismdetector.analyzer.representation.TokenizedSource
+import org.danilopianini.plagiarismdetector.analyzer.representation.TokenizedSourceImpl
+import org.danilopianini.plagiarismdetector.analyzer.steps.JavaParser
+import org.danilopianini.plagiarismdetector.analyzer.steps.JavaPreprocessor
+import org.danilopianini.plagiarismdetector.analyzer.steps.StepHandler
+import org.danilopianini.plagiarismdetector.analyzer.steps.JavaTokenizer
+import java.io.File
+
+/**
+ * A concrete [Analyzer] which executes source code tokenization.
+ */
+class TokenizationAnalyzer : Analyzer<File, TokenizedSource, Sequence<Token>> {
+    private val pipeline: StepHandler<File, Sequence<Token>> = StepHandler {
+        JavaTokenizer().process(
+            JavaPreprocessor().process(
+                JavaParser().process(it)
+            )
+        )
+    }
+
+    override fun execute(input: File): TokenizedSource = TokenizedSourceImpl(input, pipeline.process(input))
+}
