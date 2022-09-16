7b4dfcd836e97316a2985e69ed7f0c84bfe8e808
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-16 16:11:38 +0200

    feat(steps): add tokenization step handlers

---
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/steps/JavaPreprocessor.kt
@@ -0,0 +1,15 @@
+package org.danilopianini.plagiarismdetector.analyzer.steps
+
+import com.github.javaparser.ast.CompilationUnit
+import com.github.javaparser.ast.ImportDeclaration
+
+/**
+ * A java source file preprocessor.
+ */
+class JavaPreprocessor : StepHandler<CompilationUnit, CompilationUnit> {
+    override fun process(input: CompilationUnit): CompilationUnit {
+        input.removePackageDeclaration()
+        input.findAll(ImportDeclaration::class.java).forEach { input.remove(it) }
+        return input
+    }
+}
---
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/steps/StepHandler.kt
@@ -0,0 +1,14 @@
+package org.danilopianini.plagiarismdetector.analyzer.steps
+
+/**
+ * An interface modeling a step handler in the analyzer pipeline.
+ * @param I the type of the input.
+ * @param O the type of the output.
+ */
+fun interface StepHandler<I, O> {
+    /**
+     * Process the given input.
+     * @return the input transformation result.
+     */
+    fun process(input: I): O
+}
