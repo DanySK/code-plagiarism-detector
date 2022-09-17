68afc0b00d6afd1486db4d7f8f45c09bbd1efb84
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-17 13:32:11 +0200

    style: imports reordering

--- src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/TokenizationAnalyzer.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/TokenizationAnalyzer.kt
@@ -1,12 +1,12 @@
 package org.danilopianini.plagiarismdetector.analyzer
 
-import org.danilopianini.plagiarismdetector.analyzer.representation.token.Token
 import org.danilopianini.plagiarismdetector.analyzer.representation.TokenizedSource
 import org.danilopianini.plagiarismdetector.analyzer.representation.TokenizedSourceImpl
+import org.danilopianini.plagiarismdetector.analyzer.representation.token.Token
 import org.danilopianini.plagiarismdetector.analyzer.steps.JavaParser
 import org.danilopianini.plagiarismdetector.analyzer.steps.JavaPreprocessor
-import org.danilopianini.plagiarismdetector.analyzer.steps.StepHandler
 import org.danilopianini.plagiarismdetector.analyzer.steps.JavaTokenizer
+import org.danilopianini.plagiarismdetector.analyzer.steps.StepHandler
 import java.io.File
 
 /**
