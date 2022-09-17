5c1ad33f913d7abd81921431b71dc06574523000
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-17 08:41:45 +0200

    refactor(token): remove not useful sealed declaration

--- src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/representation/token/TokenType.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/representation/token/TokenType.kt
@@ -1,15 +1,12 @@
 package org.danilopianini.plagiarismdetector.analyzer.representation.token
 
-import kotlinx.serialization.Serializable
-
 /**
  * An interface modeling a token type, e.g. `keyword`, `identifier`...
  * A token type is associated with one or more language-specific constructs.
  * In the Java programming language, for example, the TokenType `loop-stmt`
  * is associated to the following: `ForEachStmt`, `ForStmt`, `WhileStmt`, `DoStmt`.
  */
-@Serializable
-sealed interface TokenType {
+interface TokenType {
     /**
      * The name of the token type.
      */
