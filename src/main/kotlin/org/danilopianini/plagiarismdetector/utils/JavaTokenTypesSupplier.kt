3fd876957e60675313de864d0419d1ea47adcfb0
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-17 18:05:20 +0200

    refactor: extract in a strategy interface token types retrieval

---
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/utils/JavaTokenTypesSupplier.kt
@@ -0,0 +1,23 @@
+package org.danilopianini.plagiarismdetector.utils
+
+import com.charleskorn.kaml.Yaml
+import kotlinx.serialization.builtins.ListSerializer
+import org.danilopianini.plagiarismdetector.analyzer.representation.token.LanguageTokenTypes
+import org.danilopianini.plagiarismdetector.analyzer.representation.token.TokenTypeImpl
+import org.danilopianini.plagiarismdetector.analyzer.representation.token.LanguageTokenTypesImpl
+
+/**
+ * A supplier of [LanguageTokenTypesImpl].
+ */
+class JavaTokenTypesSupplier : LanguageTokenTypesSupplier {
+    companion object {
+        private const val CONFIG_FILE_NAME = "java-token-types.yml"
+    }
+
+    override val types: LanguageTokenTypes
+        get() {
+            val configFile = ClassLoader.getSystemResourceAsStream(CONFIG_FILE_NAME)!!
+            val tokenTypes = Yaml.default.decodeFromStream(ListSerializer(TokenTypeImpl.serializer()), configFile)
+            return LanguageTokenTypesImpl(tokenTypes.asSequence())
+        }
+}
