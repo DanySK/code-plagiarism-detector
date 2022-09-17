367b67de6e6580eaa0e81357c9c31b92663428c8
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-17 15:00:42 +0200

    docs: improve doc comments

--- src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/steps/JavaPreprocessor.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/steps/JavaPreprocessor.kt
@@ -4,7 +4,9 @@ import com.github.javaparser.ast.CompilationUnit
 import com.github.javaparser.ast.ImportDeclaration
 
 /**
- * A java source file preprocessor.
+ * A java source file preprocessor, which removes noisy information
+ * (import and packages declarations) that can affect the overall detection
+ * accuracy of the system.
  */
 class JavaPreprocessor : StepHandler<CompilationUnit, CompilationUnit> {
     override fun process(input: CompilationUnit): CompilationUnit {
