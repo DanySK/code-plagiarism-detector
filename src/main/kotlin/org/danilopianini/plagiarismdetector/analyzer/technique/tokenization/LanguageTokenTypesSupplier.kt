2ba92ca48f764e04326bca1347a8eef1940bea8a
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-17 20:02:55 +0200

    docs: improve doc comments

--- src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/technique/tokenization/LanguageTokenTypesSupplier.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/technique/tokenization/LanguageTokenTypesSupplier.kt
@@ -3,11 +3,13 @@ package org.danilopianini.plagiarismdetector.analyzer.technique.tokenization
 import org.danilopianini.plagiarismdetector.analyzer.representation.token.LanguageTokenTypes
 
 /**
- * A supplier of [LanguageTokenTypes].
+ * A supplier of [LanguageTokenTypes], an object responsible for
+ * retrieving the lexical token types of a specific programming
+ * language and encapsulating them in a [LanguageTokenTypes].
  */
 interface LanguageTokenTypesSupplier {
     /**
-     * The token types object.
+     * The [LanguageTokenTypes] object.
      */
     val types: LanguageTokenTypes
 }
