e348e5f4c7aaead7b0978ef634c4d3c14920ec57
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-19 11:12:11 +0200

    docs: add constructor parameter doc

--- src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/representation/token/LanguageTokenTypesImpl.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/representation/token/LanguageTokenTypesImpl.kt
@@ -2,6 +2,7 @@ package org.danilopianini.plagiarismdetector.analyzer.representation.token
 
 /**
  * A simple class implementing a [LanguageTokenTypes].
+ * @param tokensTypes the set of token types of the language
  */
 class LanguageTokenTypesImpl(private val tokensTypes: Collection<TokenType>) : LanguageTokenTypes {
     override fun tokenFor(constructName: String): TokenType {
