1ef030ba13ec4dca604c25023c7c317f9b81412e
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-24 14:36:15 +0200

    test: replace env variable test with system extensions

--- src/test/kotlin/org/danilopianini/plagiarismdetector/provider/TestConfigurationUtils.kt
+++
@@ -1,5 +0,0 @@
-package org.danilopianini.plagiarismdetector.provider
-
-private const val PR_BUILD_VARIABLE = "PR_BUILD"
-
-fun isExecutingOnPullRequest() = System.getenv(PR_BUILD_VARIABLE) == "true"
