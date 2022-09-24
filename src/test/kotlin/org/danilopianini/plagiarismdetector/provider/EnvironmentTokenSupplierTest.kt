df8405b17d2da43a3ccdbd44eb15671422ed6663
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-24 10:32:35 +0200

    test: add test for the environment token supplier

---
+++ src/test/kotlin/org/danilopianini/plagiarismdetector/provider/EnvironmentTokenSupplierTest.kt
@@ -0,0 +1,30 @@
+package org.danilopianini.plagiarismdetector.provider
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.string.shouldNotBeEmpty
+import org.danilopianini.plagiarismdetector.provider.authentication.EnvironmentTokenSupplier
+
+class EnvironmentTokenSupplierTest : FunSpec() {
+    companion object {
+        private const val BB_AUTH_USER_VAR = "BB_USER"
+        private const val BB_AUTH_TOKEN_VAR = "BB_TOKEN"
+        private const val NON_EXISTING_TOKEN_VAR = "WRONG_TOKEN"
+    }
+
+    init {
+        test("Trying to retrieve the content of a non-existing environment variable should throw an exception") {
+            val supplier = EnvironmentTokenSupplier(NON_EXISTING_TOKEN_VAR)
+            shouldThrow<IllegalArgumentException> {
+                supplier.token
+            }
+        }
+
+        test("The content of an existing environment variable should meet the formatting requirements") {
+            val supplier = EnvironmentTokenSupplier(BB_AUTH_USER_VAR, BB_AUTH_TOKEN_VAR, separator = ":")
+            supplier.token.shouldNotBeEmpty()
+            supplier.token.shouldContain(Regex("(.+):(.+)"))
+        }
+    }
+}
