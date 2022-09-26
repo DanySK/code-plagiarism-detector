9d7a4e1762928e1ad5b64d3bfc3d9d413a333680
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-26 14:43:08 +0200

    style: better formatting

--- src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/representation/token/LanguageTokenTypesImpl.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/representation/token/LanguageTokenTypesImpl.kt
@@ -5,6 +5,7 @@ package org.danilopianini.plagiarismdetector.analyzer.representation.token
  * @param tokensTypes the set of token types of the language
  */
 class LanguageTokenTypesImpl(private val tokensTypes: Collection<TokenType>) : LanguageTokenTypes {
+
     override fun tokenFor(constructName: String): TokenType {
         require(isToken(constructName))
         return tokensTypes.find { it.languageConstructs.contains(constructName) }!!
