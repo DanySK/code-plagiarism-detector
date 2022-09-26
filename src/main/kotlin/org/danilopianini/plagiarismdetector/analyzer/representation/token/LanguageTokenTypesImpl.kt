7600bed2f8d433805d06f6b2be35ee0addb57b65
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-26 21:48:52 +0200

    refactor: replace collection with set

--- src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/representation/token/LanguageTokenTypesImpl.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/analyzer/representation/token/LanguageTokenTypesImpl.kt
@@ -4,7 +4,7 @@ package org.danilopianini.plagiarismdetector.analyzer.representation.token
  * A simple class implementing a [LanguageTokenTypes].
  * @param tokensTypes the set of token types of the language
  */
-class LanguageTokenTypesImpl(private val tokensTypes: Collection<TokenType>) : LanguageTokenTypes {
+class LanguageTokenTypesImpl(private val tokensTypes: Set<TokenType>) : LanguageTokenTypes {
 
     override fun tokenFor(constructName: String): TokenType =
         tokensTypes.find { constructName in it.languageConstructs } ?: error(
