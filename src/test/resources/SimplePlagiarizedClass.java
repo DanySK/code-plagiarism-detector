90d844de2011872793854e5c47a8edbf73da5a48
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-09-29 22:20:00 +0200

    chore(testing class): changes in plagiarized source

--- src/test/resources/SimplePlagiarizedClass.java
+++ src/test/resources/SimplePlagiarizedClass.java
@@ -1,19 +1,17 @@
 package org.example.javaparser.samples.Test;
 
-import java.util.Random;
-import java.util.stream.IntStream;
-
 /**
- * This is a testing class.
+ * This is a plagiarized testing class.
  */
 public class SimpleTestClass { // test class
     public static void main(String[] args) {
-        var k = (new Random()).nextInt(1500);
-        IntStream.range(0, 1000).forEach( i -> {
+        Integer k = (new java.util.Random()).nextInt(1000);
+        for (int i = 0; i < 100; i++) {
             k++;
-            if (!k%5) {
+            if (k % 5 == 0) {
                 System.out.println(k + "5 is 0!");
             }
-        });
+        }
     }
 }
+
