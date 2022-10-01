2ffa4bb2259c0f318e1e4054f48ce1519f7c574b
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-10-01 17:22:54 +0200

    test(detector): add RKR detector test

--- src/test/kotlin/org/danilopianini/plagiarismdetector/detector/TokenBasedPlagiarismDetectorTest.kt
+++ src/test/kotlin/org/danilopianini/plagiarismdetector/detector/TokenBasedPlagiarismDetectorTest.kt
@@ -1,8 +1,10 @@
 package org.danilopianini.plagiarismdetector.detector
 
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.comparables.shouldBeGreaterThan
+import io.kotest.matchers.doubles.shouldBeExactly
+import io.kotest.mpp.timeInMillis
 import org.danilopianini.plagiarismdetector.analyzer.technique.tokenization.java.JavaTokenizationAnalyzer
+import org.danilopianini.plagiarismdetector.detector.technique.tokenization.RKRGreedyStringTiling
 import org.danilopianini.plagiarismdetector.detector.technique.tokenization.TokenBasedPlagiarismDetector
 import org.danilopianini.plagiarismdetector.detector.technique.tokenization.TokenMatch
 import org.slf4j.LoggerFactory
@@ -14,21 +16,30 @@ class TokenBasedPlagiarismDetectorTest : FunSpec() {
         private const val PLAGIARIZED_FILE = "SimplePlagiarizedClass.java"
     }
     private val logger = LoggerFactory.getLogger(this::class.java)
-    private val fileSourcePath = ClassLoader.getSystemResource(SOURCE_FILE)
-    private val filePlagiarizedPath = ClassLoader.getSystemResource(PLAGIARIZED_FILE)
+    private val sourceFile = File(ClassLoader.getSystemResource(SOURCE_FILE).toURI())
+    private val plagiarizedFile = File(ClassLoader.getSystemResource(PLAGIARIZED_FILE).toURI())
     private val analyzer = JavaTokenizationAnalyzer()
 
     init {
-        test("checking similarity") {
-            val sourceFile = File(fileSourcePath.toURI())
-            val plagiarizedFile = File(filePlagiarizedPath.toURI())
-            val result = TokenBasedPlagiarismDetector().invoke(
-                Pair(analyzer(sourceFile), analyzer(plagiarizedFile))
-            )
-            logger.info("Score of similarity: ${result.scoreOfSimilarity}")
-            logger.info("Matches:")
-            result.matches.map(TokenMatch::toString).forEach(logger::info)
-            result.scoreOfSimilarity shouldBeGreaterThan 0.5
+        test("checking similarity between ${sourceFile.name} and ${plagiarizedFile.name}") {
+            val (GSTElapsedTime, GSTResult) = detect(TokenBasedPlagiarismDetector())
+            logger.info("> Greedy String Tiling")
+            logger.info(">> Elapsed time: $GSTElapsedTime ms")
+            logger.info(">> Score of similarity: ${GSTResult.scoreOfSimilarity}")
+            // logger.info(">> Matches: ${GSTResult.matches.map(TokenMatch::toString).forEach(logger::info)}")
+            val (RKRElapsedTime, RKRResult) = detect(TokenBasedPlagiarismDetector(RKRGreedyStringTiling()))
+            logger.info("> Running-Karp-Rabin Greedy String Tiling")
+            logger.info(">> Elapsed time: $RKRElapsedTime ms")
+            logger.info(">> Score of similarity: ${RKRResult.scoreOfSimilarity}")
+            // logger.info(">> Matches: ${RKRResult.matches.map(TokenMatch::toString).forEach(logger::info)}")
+            GSTResult.scoreOfSimilarity shouldBeExactly RKRResult.scoreOfSimilarity
         }
+    }
+
+    private fun detect(detector: TokenBasedPlagiarismDetector): Pair<Long, ComparisonResult<TokenMatch>> {
+        val start = timeInMillis()
+        val result = detector(Pair(analyzer(sourceFile), analyzer(plagiarizedFile)))
+        val stop = timeInMillis()
+        return Pair(stop - start, result)
     }
 }
