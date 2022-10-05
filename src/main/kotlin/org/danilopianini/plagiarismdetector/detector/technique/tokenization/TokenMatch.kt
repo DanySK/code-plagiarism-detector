ea5cfd48620d2aa0f0c5bae36fca6e118f613630
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-10-05 15:53:06 +0200

    docs(match): add doc details

--- src/main/kotlin/org/danilopianini/plagiarismdetector/detector/technique/tokenization/TokenMatch.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/detector/technique/tokenization/TokenMatch.kt
@@ -5,16 +5,16 @@ import org.danilopianini.plagiarismdetector.analyzer.representation.token.Token
 import org.danilopianini.plagiarismdetector.detector.Match
 
 /**
- * A match between two files.
+ * A match between two [TokenizedSource].
  */
 interface TokenMatch : Match {
     /**
-     * The matching [Token] in the pattern.
+     * The matching [Token]s in the pattern, i.e. the shorter [TokenizedSource].
      */
     val pattern: Pair<TokenizedSource, List<Token>>
 
     /**
-     * The matching [Token] in the text.
+     * The matching [Token]s in the text, i.e. the longer [TokenizedSource].
      */
     val text: Pair<TokenizedSource, List<Token>>
 
