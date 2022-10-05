c7cfde8ba9c039d05e2512be17620045ee74cef0
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-10-05 16:41:08 +0200

    refactor(gst): scoreOfSimilarity -> similarity

--- src/main/kotlin/org/danilopianini/plagiarismdetector/detector/technique/tokenization/TokenBasedComparisonResult.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/detector/technique/tokenization/TokenBasedComparisonResult.kt
@@ -6,7 +6,7 @@ import org.danilopianini.plagiarismdetector.detector.ComparisonResult
  * An implementation of [ComparisonResult].
  */
 class TokenBasedComparisonResult(
-    override val scoreOfSimilarity: Double,
+    override val similarity: Double,
     tokenMatches: Set<TokenMatch>,
 ) : ComparisonResult<TokenMatch> {
 
