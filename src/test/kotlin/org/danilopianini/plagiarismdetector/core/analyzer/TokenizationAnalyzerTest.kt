43d9eac30012fddae5214f2ed793edec7f3e82f7
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-11-02 17:44:00 +0100

    test(analyzer): merge tokenization and preprocessor test

--- src/test/kotlin/org/danilopianini/plagiarismdetector/core/analyzer/TokenizationAnalyzerTest.kt
+++ src/test/kotlin/org/danilopianini/plagiarismdetector/core/analyzer/TokenizationAnalyzerTest.kt
@@ -1,27 +1,41 @@
 package org.danilopianini.plagiarismdetector.core.analyzer
 
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.comparables.shouldBeGreaterThan
+import io.kotest.matchers.collections.shouldNotBeEmpty
+import io.kotest.matchers.ints.shouldBeExactly
+import io.kotest.matchers.ints.shouldBeInRange
 import io.kotest.matchers.shouldBe
 import org.danilopianini.plagiarismdetector.core.analyzer.technique.tokenization.java.JavaTokenizationAnalyzer
-import org.slf4j.LoggerFactory
 import java.io.File
 
 class TokenizationAnalyzerTest : FunSpec() {
-    companion object {
-        private const val SIMPLE_FILE_NAME = "CaveGenerator.java"
-    }
-    private val logger = LoggerFactory.getLogger(this::class.java)
-    private val filePath = ClassLoader.getSystemResource(SIMPLE_FILE_NAME)
+
+    private val sourceFile = File(ClassLoader.getSystemResource(FILE_NAME).toURI())
     private val analyzer = JavaTokenizationAnalyzer()
 
     init {
+        test("Processing phase should remove imports and package declarations, equals and hashcode functions") {
+            val result = JavaTokenizationAnalyzer()(sourceFile).representation.toList()
+            result.shouldNotBeEmpty()
+            result.forEach {
+                it.line shouldBeInRange IntRange(
+                    FIRST_LINE_CLASS,
+                    LAST_LINE_CLASS_WITHOUT_EQUALS_AND_HASHCODE
+                )
+            }
+        }
+
         test("Tokenizing source code should return a tokenized representation of source code") {
-            val sourceFile = File(filePath.toURI())
             val result = analyzer(sourceFile)
             result.sourceFile.path shouldBe sourceFile.path
-            result.representation.count() shouldBeGreaterThan 0
-            result.representation.forEach { logger.info(it.toString()) }
+            result.representation.count() shouldBeExactly EXPECTED_TOKENS_NUMBER
         }
+    }
+
+    companion object {
+        private const val FILE_NAME = "TestAnalyzer.java"
+        private const val FIRST_LINE_CLASS = 6
+        private const val LAST_LINE_CLASS_WITHOUT_EQUALS_AND_HASHCODE = 16
+        private const val EXPECTED_TOKENS_NUMBER = 14
     }
 }
