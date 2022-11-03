d4e35fb7507e9d79f1d381410380ac57bd690339
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-11-03 16:58:14 +0100

    test(analyzer): add test case for sources with errors

--- src/test/kotlin/org/danilopianini/plagiarismdetector/core/analyzer/TokenizationAnalyzerTest.kt
+++ src/test/kotlin/org/danilopianini/plagiarismdetector/core/analyzer/TokenizationAnalyzerTest.kt
@@ -1,21 +1,40 @@
 package org.danilopianini.plagiarismdetector.core.analyzer
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.engine.spec.tempfile
 import io.kotest.matchers.collections.shouldNotBeEmpty
 import io.kotest.matchers.ints.shouldBeExactly
 import io.kotest.matchers.ints.shouldBeInRange
 import io.kotest.matchers.shouldBe
+import org.danilopianini.plagiarismdetector.core.analyzer.technique.tokenization.java.JavaParser
 import org.danilopianini.plagiarismdetector.core.analyzer.technique.tokenization.java.JavaTokenizationAnalyzer
 import java.io.File
 
 class TokenizationAnalyzerTest : FunSpec() {
 
-    private val sourceFile = File(ClassLoader.getSystemResource(FILE_NAME).toURI())
     private val analyzer = JavaTokenizationAnalyzer()
 
     init {
+        test("**Parsing** source file with errors should skip it and print an error message on logger") {
+            val sourceFileWithError = tempfile()
+            sourceFileWithError.writeText(
+                """
+                public class ClassWithErrors {
+                    public static void main() {
+                        System.out.println("Hello World")   // This line does not end with semicolon!
+                    }
+                }
+                """.trimIndent()
+            )
+            shouldThrow<IllegalStateException> {
+                JavaParser()(sourceFileWithError)
+            }
+        }
+
         test("Processing phase should remove imports and package declarations, equals and hashcode functions") {
-            val result = JavaTokenizationAnalyzer()(sourceFile).representation.toList()
+            val sourceFile = File(ClassLoader.getSystemResource(FILE_NAME).toURI())
+            val result = analyzer(sourceFile).representation.toList()
             result.shouldNotBeEmpty()
             result.forEach {
                 it.line shouldBeInRange IntRange(
@@ -26,6 +45,7 @@ class TokenizationAnalyzerTest : FunSpec() {
         }
 
         test("Tokenizing source code should return a tokenized representation of source code") {
+            val sourceFile = File(ClassLoader.getSystemResource(FILE_NAME).toURI())
             val result = analyzer(sourceFile)
             result.sourceFile.path shouldBe sourceFile.path
             result.representation.count() shouldBeExactly EXPECTED_TOKENS_NUMBER
