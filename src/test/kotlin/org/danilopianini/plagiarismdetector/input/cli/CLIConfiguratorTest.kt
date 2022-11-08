caaf6bfec1efc364cccae8343c23c4dfe6016e1e
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-11-08 18:02:41 +0100

    refactor(repository): throw an illegal argument exception with explanatory message if corpus or submission subcommand is not valued

--- src/test/kotlin/org/danilopianini/plagiarismdetector/input/cli/CLIConfiguratorTest.kt
+++ src/test/kotlin/org/danilopianini/plagiarismdetector/input/cli/CLIConfiguratorTest.kt
@@ -1,7 +1,9 @@
 package org.danilopianini.plagiarismdetector.input.cli
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.engine.spec.tempdir
+import io.kotest.matchers.string.shouldStartWith
 
 class CLIConfiguratorTest : FunSpec({
 
@@ -24,5 +26,22 @@ class CLIConfiguratorTest : FunSpec({
             "github,bitbucket"
         )
         CLIConfigurator().sessionFrom(args)
+    }
+
+    test(
+        "If one between `corpus` and `provider` command is not provided, " +
+            "an `IllegalArgument` exception with explanatory message is thrown"
+    ) {
+        val args = listOf(
+            "--output-dir",
+            tempdir().path,
+            "submission",
+            "--url",
+            "https://github.com/DanySK/code-plagiarism-detector",
+        )
+        val exception = shouldThrow<IllegalArgumentException> {
+            CLIConfigurator().sessionFrom(args)
+        }
+        exception.message shouldStartWith "Both `corpus` and `provider` subcommands are required"
     }
 })
