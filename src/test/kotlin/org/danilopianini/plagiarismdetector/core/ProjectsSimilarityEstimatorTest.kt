c2b862fe5ee6716368be9ecb1f0c70e95cbdf773
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-11-09 14:53:28 +0100

    refactor(core): if matches is empty return 0 and not compute percentile cause this return nan

---
+++ src/test/kotlin/org/danilopianini/plagiarismdetector/core/ProjectsSimilarityEstimatorTest.kt
@@ -0,0 +1,45 @@
+package org.danilopianini.plagiarismdetector.core
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.doubles.plusOrMinus
+import io.kotest.matchers.doubles.shouldBeExactly
+import io.kotest.matchers.shouldBe
+import io.mockk.mockk
+import org.danilopianini.plagiarismdetector.core.detector.ComparisonResult
+import org.danilopianini.plagiarismdetector.core.detector.technique.tokenization.TokenBasedComparisonResult
+import org.danilopianini.plagiarismdetector.core.detector.technique.tokenization.TokenMatch
+
+/**
+ * A test class for testing project similarity estimations.
+ *
+ * Note: to know how percentile value is calculated see [`UnivariateStatistic` java doc](shorturl.at/egozH)
+ */
+class ProjectsSimilarityEstimatorTest : FunSpec({
+
+    test("Testing similarity estimation between projects with no matches") {
+        val report = ReportImpl(
+            mockk(),
+            mockk(),
+            emptySet<ComparisonResult<TokenMatch>>(),
+            0.0
+        )
+        report.similarity shouldBeExactly 0.0
+    }
+
+    test("Testing similarity estimation between projects with a single matches") {
+        val reportedRatio = 0.5
+        val report = ReportImpl(
+            mockk(),
+            mockk(),
+            setOf(
+                TokenBasedComparisonResult(0.63, emptySet()),
+                TokenBasedComparisonResult(1.0, emptySet()),
+                TokenBasedComparisonResult(0.44, emptySet()),
+                TokenBasedComparisonResult(0.78, emptySet()),
+            ),
+            reportedRatio
+        )
+        val percentileValue = 0.945
+        report.similarity.shouldBe(reportedRatio * percentileValue plusOrMinus 0.00001)
+    }
+})
