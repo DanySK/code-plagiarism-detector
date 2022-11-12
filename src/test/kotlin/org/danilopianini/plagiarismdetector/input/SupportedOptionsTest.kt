62f4160bb8cf0fe6a86cf9bed2f9ed305ed4fdee
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-11-12 15:42:37 +0100

    refactor(options): extracted common utilities functions in SupportedOptions object

---
+++ src/test/kotlin/org/danilopianini/plagiarismdetector/input/SupportedOptionsTest.kt
@@ -0,0 +1,28 @@
+package org.danilopianini.plagiarismdetector.input
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import org.danilopianini.plagiarismdetector.commons.BitBucket
+import org.danilopianini.plagiarismdetector.commons.GitHub
+import java.net.URL
+
+class SupportedOptionsTest : FunSpec({
+
+    test("Searching unsupported service should throw exception") {
+        shouldThrow<java.lang.IllegalArgumentException> {
+            SupportedOptions.serviceBy("non-existing-service")
+            SupportedOptions.serviceBy(URL("https://non-existing-service.org"))
+        }
+    }
+
+    test("Test get service by its name") {
+        SupportedOptions.serviceBy("github") shouldBe GitHub
+        SupportedOptions.serviceBy("bitbucket") shouldBe BitBucket
+    }
+
+    test("Test get service by url") {
+        SupportedOptions.serviceBy(URL("https://github.com/orgs/unibo-oop-projects")) shouldBe GitHub
+        SupportedOptions.serviceBy(URL("https://bitbucket.org/danysk/")) shouldBe BitBucket
+    }
+})
