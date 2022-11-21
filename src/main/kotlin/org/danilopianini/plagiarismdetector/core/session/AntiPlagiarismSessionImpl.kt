759bca71ad6d0e7b8962a446cebdba90c6fe2e78
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-11-21 11:01:13 +0100

    refactor: replace log prints with output

--- src/main/kotlin/org/danilopianini/plagiarismdetector/core/session/AntiPlagiarismSessionImpl.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/core/session/AntiPlagiarismSessionImpl.kt
@@ -4,10 +4,9 @@ import org.danilopianini.plagiarismdetector.core.Report
 import org.danilopianini.plagiarismdetector.core.ReportImpl
 import org.danilopianini.plagiarismdetector.core.detector.Match
 import org.danilopianini.plagiarismdetector.input.configuration.RunConfiguration
+import org.danilopianini.plagiarismdetector.output.StandardOutput
 import org.danilopianini.plagiarismdetector.repository.Repository
-import org.slf4j.LoggerFactory
 import java.util.stream.Collectors
-import kotlin.system.measureTimeMillis
 
 /**
  * A **parallel** implementation of an [AntiPlagiarismSession].
@@ -16,29 +15,29 @@ class AntiPlagiarismSessionImpl<out C : RunConfiguration<M>, M : Match>(
     private val configuration: C
 ) : AntiPlagiarismSession {
 
-    private val logger = LoggerFactory.getLogger(this.javaClass)
+    private val output = StandardOutput()
     private val processedResult = mutableSetOf<Report<M>>()
 
-    override operator fun invoke() = logExecutionTime {
-        with(configuration) {
-            submission.forEach { submission ->
-                val result = processNotYetProcessed(submission).toMutableSet()
-                result.addAll(retrieveAlreadyProcessed(submission))
-                processedResult.addAll(result)
-                if (result.isNotEmpty()) {
-                    exporter(result)
-                }
+    override operator fun invoke() = with(configuration) {
+        submission.forEach { submission ->
+            val result = processNotYetProcessed(submission).toMutableSet()
+            result.addAll(retrieveAlreadyProcessed(submission))
+            processedResult.addAll(result)
+            if (result.isNotEmpty()) {
+                exporter(result)
             }
         }
     }
 
     private fun processNotYetProcessed(submission: Repository): Set<Report<M>> = with(configuration) {
+        output.startComparison(submission.name, configuration.corpus.count())
         corpus.filter { it.name != submission.name && submission hasNotYetComparedAgainst it }
             .toSet()
             .parallelStream()
+            .peek { output.tick() }
             .map { technique.execute(submission, it, filesToExclude, minDuplicatedPercentage) }
             .collect(Collectors.toSet())
-    }
+    }.also { output.endComparison() }
 
     private fun retrieveAlreadyProcessed(submission: Repository): Set<Report<M>> = with(configuration) {
         corpus.mapNotNull { corpus ->
@@ -54,16 +53,4 @@ class AntiPlagiarismSessionImpl<out C : RunConfiguration<M>, M : Match>(
     private fun Report<M>.refersTo(repo1: Repository, repo2: Repository): Boolean =
         (comparedProject.name == repo1.name && submittedProject.name == repo2.name) ||
             (comparedProject.name == repo2.name && submittedProject.name == repo1.name)
-
-    /**
-     * Logs the elapsed time used to execute the given [block].
-     */
-    private fun logExecutionTime(block: () -> Unit) {
-        val elapsedTime = measureTimeMillis(block)
-        logger.info("> Elapsed time: ${elapsedTime.toDouble() / MILLISECONDS_IN_ONE_MINUTE} min.")
-    }
-
-    companion object {
-        private const val MILLISECONDS_IN_ONE_MINUTE = 60_000
-    }
 }
