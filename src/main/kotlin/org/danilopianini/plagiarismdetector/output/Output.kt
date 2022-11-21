70bf895678357b21cde3907f0157117e6cadd947
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-11-21 09:49:39 +0100

    feat(output): add standard output

---
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/output/Output.kt
@@ -0,0 +1,30 @@
+package org.danilopianini.plagiarismdetector.output
+
+/**
+ * An interface modeling a resource on which is possible
+ * to present output results.
+ */
+interface Output {
+
+    /**
+     * Notifies the UI that the comparison of [submissionName] has started
+     * and will last the given number of [totalComparisons].
+     */
+    fun startComparison(submissionName: String, totalComparisons: Int)
+
+    /**
+     * Notifies the UI a new comparison has been completed since the
+     * comparison process started.
+     */
+    fun tick()
+
+    /**
+     * Notifies the UI the comparison process is ended.
+     */
+    fun endComparison()
+
+    /**
+     * Logs a [message] on the UI.
+     */
+    fun logInfo(message: String)
+}
