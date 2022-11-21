2de74fa9de6834941a2a11bd0150d08f2db9206e
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-11-21 11:38:44 +0100

    test(cli): add listener to intercept exit process event

--- src/test/kotlin/org/danilopianini/plagiarismdetector/input/cli/CLIConfiguratorTest.kt
+++ src/test/kotlin/org/danilopianini/plagiarismdetector/input/cli/CLIConfiguratorTest.kt
@@ -5,65 +5,66 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.engine.spec.tempdir
 import io.kotest.engine.spec.tempfile
-import io.kotest.matchers.string.shouldStartWith
+import io.kotest.extensions.system.SpecSystemExitListener
+import io.kotest.extensions.system.SystemExitException
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.spyk
 import org.danilopianini.plagiarismdetector.provider.BitbucketProvider
 import org.danilopianini.plagiarismdetector.provider.GitHubProvider
 
-class CLIConfiguratorTest : FunSpec({
+class CLIConfiguratorTest : FunSpec() {
 
-    val configurator = spyk<CLIConfigurator>()
-    every { configurator getProperty "github" } propertyType GitHubProvider::class answers {
-        GitHubProvider.connectAnonymously()
-    }
-    every { configurator getProperty "bitbucket" } propertyType BitbucketProvider::class answers {
-        BitbucketProvider.connectAnonymously()
-    }
+    override fun listeners() = listOf(SpecSystemExitListener)
 
-    test(
-        "Launching CLI configuration with multiple users which are not" +
-            " present in both services should not throw an exception"
-    ) {
-        val args = listOf(
-            "--output-dir",
-            tempdir().path,
-            "submission",
-            "--url",
-            "https://github.com/DanySK/code-plagiarism-detector",
-            "corpus",
-            "--repository-name",
-            "oop",
-            "--user",
-            "danysk,unibo-oop-projects",
-            "--service",
-            "github,bitbucket"
-        )
-        configurator.sessionFrom(args)
-    }
+    init {
+        val configurator = spyk<CLIConfigurator>()
+        every { configurator getProperty "github" } propertyType GitHubProvider::class answers {
+            GitHubProvider.connectAnonymously()
+        }
+        every { configurator getProperty "bitbucket" } propertyType BitbucketProvider::class answers {
+            BitbucketProvider.connectAnonymously()
+        }
 
-    test(
-        "If one between `corpus` and `provider` command is not provided, " +
-            "an `IllegalArgument` exception with explanatory message is thrown"
-    ) {
-        val args = listOf(
-            "--output-dir",
-            tempdir().path,
-            "submission",
-            "--url",
-            "https://github.com/DanySK/code-plagiarism-detector",
-        )
-        val exception = shouldThrow<IllegalArgumentException> {
+        test(
+            "Launching CLI configuration with multiple users which are not" +
+                " present in both services should not throw an exception"
+        ) {
+            val args = listOf(
+                "--output-dir",
+                tempdir().path,
+                "submission",
+                "--url",
+                "https://github.com/DanySK/code-plagiarism-detector",
+                "corpus",
+                "--repository-name",
+                "oop",
+                "--user",
+                "danysk,unibo-oop-projects",
+                "--service",
+                "github,bitbucket"
+            )
             configurator.sessionFrom(args)
         }
-        exception.message shouldStartWith "Both `corpus` and `provider` subcommands are required"
-    }
 
-    test("If output path is not a directory should throw an exception") {
-        val outputFile = tempfile()
-        val args = listOf("--o", outputFile.path)
-        shouldThrow<BadParameterValue> {
-            CLI().parse(args)
+        test("If `corpus` or `provider` command is not provided, the process exits with an error message") {
+            val args = listOf(
+                "--output-dir",
+                tempdir().path,
+                "submission",
+                "--url",
+                "https://github.com/DanySK/code-plagiarism-detector"
+            )
+            val thrown = shouldThrow<SystemExitException> { configurator.sessionFrom(args) }
+            thrown.exitCode shouldBe 1
+        }
+
+        test("If output path is not a directory should throw an exception") {
+            val outputFile = tempfile()
+            val args = listOf("--o", outputFile.path)
+            shouldThrow<BadParameterValue> {
+                CLI().parse(args)
+            }
         }
     }
-})
+}
