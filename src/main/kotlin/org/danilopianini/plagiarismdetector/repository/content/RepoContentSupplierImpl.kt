62c902adcb3e3555e5a3ce20f5bf9ca80b9e1144
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-11-25 14:56:46 +0100

    refactor(repository): more coincise class names

--- src/main/kotlin/org/danilopianini/plagiarismdetector/repository/content/RepoContentSupplierImpl.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/repository/content/RepoContentSupplierImpl.kt
@@ -4,16 +4,19 @@ import org.apache.commons.io.FileUtils
 import org.apache.commons.io.filefilter.DirectoryFileFilter
 import org.apache.commons.io.filefilter.RegexFileFilter
 import org.danilopianini.plagiarismdetector.caching.FileKnowledgeBaseManager
+import org.danilopianini.plagiarismdetector.caching.KnowledgeBaseManager
 import org.danilopianini.plagiarismdetector.repository.Repository
 import java.io.File
 
 /**
- * A strategy which demands to a [FileKnowledgeBaseManager]
- * the caching of [repository] sources.
+ * A strategy that is responsible for saving and retrieving the [repository] sources
+ * using a [knowledgeBaseManager], which is by default a [FileKnowledgeBaseManager].
  */
-class RepoContentSupplierCloneStrategy(private val repository: Repository) : RepoContentSupplierStrategy {
+class RepoContentSupplierCloneStrategy(
+    private val repository: Repository,
+    private val knowledgeBaseManager: KnowledgeBaseManager = FileKnowledgeBaseManager(),
+) : RepoContentSupplierStrategy {
 
-    private val knowledgeBaseManager = FileKnowledgeBaseManager()
     private val contentDirectory by lazy {
         if (!knowledgeBaseManager.isCached(repository)) {
             knowledgeBaseManager.save(repository)
