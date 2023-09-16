85325cb209685b39476c6ba9a67a539b527efcfa
Danilo Pianini <danilo.pianini@unibo.it>
2023-09-16 16:56:17 +0200

    feat: add method do build a `Repository` from `URI`

--- src/main/kotlin/org/danilopianini/plagiarismdetector/provider/RepositoryProvider.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/provider/RepositoryProvider.kt
@@ -2,6 +2,7 @@ package org.danilopianini.plagiarismdetector.provider
 
 import org.danilopianini.plagiarismdetector.provider.criteria.SearchCriteria
 import org.danilopianini.plagiarismdetector.repository.Repository
+import java.net.URI
 import java.net.URL
 
 /**
@@ -9,6 +10,14 @@ import java.net.URL
  * @param C the type of the search criteria.
  */
 interface RepositoryProvider<I, O, in C : SearchCriteria<I, O>> {
+    /**
+     * Search a repository by the given [URI].
+     * @param uri the [URI] address in `owner/name` format of the public repository to retrieve.
+     * @return the requested [Repository] if exists.
+     * @throws IllegalArgumentException if the [uri] doesn't match the `owner/name` pattern or is not correct.
+     */
+    fun byLink(uri: URI): Repository = byLink(uri.toURL())
+
     /**
      * Search a repository by the given [URL].
      * @param url the [URL] address in `owner/name` format of the public repository to retrieve.
