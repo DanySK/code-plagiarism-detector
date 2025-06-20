c1accb392040fa5b98c5ef7e8e0b483a5f7f63a0
Danilo Pianini <danilo.pianini@unibo.it>
2025-06-20 11:08:07 +0200

    feat(bitbucket): cache accesses to reduce the necessity to throttle

--- src/main/kotlin/org/danilopianini/plagiarismdetector/provider/BitbucketProvider.kt
+++ src/main/kotlin/org/danilopianini/plagiarismdetector/provider/BitbucketProvider.kt
@@ -1,5 +1,6 @@
 package org.danilopianini.plagiarismdetector.provider
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import com.mashape.unirest.http.Unirest
 import java.net.URL
 import java.nio.charset.StandardCharsets
@@ -35,6 +36,41 @@ class BitbucketProvider private constructor(private var encodedAuthenticationTok
         private const val FORBIDDEN_CODE = 403
         private val WAIT_TIME = 1.minutes.inWholeMilliseconds
 
+        private val responses = Caffeine.newBuilder().build<Pair<String, String?>, Set<JSONObject>> { (url, token) ->
+            getResponses(url, token)
+        }
+
+        private fun getResponses(url: String, token: String?): Set<JSONObject> {
+            val responses = mutableSetOf<JSONObject>()
+            responses.add(doGETRequest(url, token))
+            check(!responses.last().hasError()) {
+                responses.last().getJSONObject(ERROR_FIELD).get(MESSAGE_FIELD).toString()
+            }
+            if (responses.last().hasNext()) {
+                responses.addAll(getResponses(responses.last().get(NEXT_PAGE_FIELD).toString(), token))
+            }
+            return responses
+        }
+
+        private tailrec fun doGETRequest(url: String, token: String?): JSONObject {
+            val request = Unirest.get(url).header(ACCEPT_HEADER_FIELD, ACCEPT_HEADER_VALUE)
+            token?.let { request.header(AUTH_HEADER_FIELD, "$AUTH_HEADER_PREFIX $it") }
+            val response = request.asBinary()
+            check(response.status != UNAUTHORIZED_CODE && response.status != FORBIDDEN_CODE) {
+                "HTTP Response: ${response.statusText}. ${IOUtils.toString(response.body, StandardCharsets.UTF_8)}"
+            }
+            val shouldBeJSON = IOUtils.toString(response.body, StandardCharsets.UTF_8)
+            val parsedResponse = runCatching { JSONObject(shouldBeJSON) }
+            if (parsedResponse.isSuccess) {
+                return parsedResponse.getOrThrow()
+            }
+            println("Error parsing response from Bitbucket: ${parsedResponse.exceptionOrNull()?.message}")
+            println("Received non-JSON payload: $shouldBeJSON")
+            println("Assuming a server-side error, throttling requests for 60 seconds.")
+            Thread.sleep(WAIT_TIME)
+            return doGETRequest(url, token)
+        }
+
         /**
          * Creates a [BitbucketProvider] with anonymous authentication: this is **not** recommended due to
          * rate limits. In case limits are reached an exception is thrown.
@@ -49,49 +85,20 @@ class BitbucketProvider private constructor(private var encodedAuthenticationTok
         fun connectWithToken(tokenSupplier: AuthenticationTokenSupplierStrategy) = BitbucketProvider(
             Base64.getEncoder().encodeToString(tokenSupplier.token.toByteArray(StandardCharsets.UTF_8)),
         )
+
+        private fun JSONObject.hasError(): Boolean = !isNull(ERROR_FIELD)
+
+        private fun JSONObject.hasNext(): Boolean = !isNull(NEXT_PAGE_FIELD)
     }
 
     override fun urlIsValid(url: URL): Boolean = url.host == BITBUCKET_HOST
 
-    override fun getRepoByUrl(url: URL): Repository = BitbucketRepository(getResponses(BASE_URL.plus(url.path)).first())
+    override fun getRepoByUrl(url: URL): Repository =
+        BitbucketRepository(responses[BASE_URL.plus(url.path) to encodedAuthenticationToken].first())
 
-    override fun byCriteria(criteria: BitbucketSearchCriteria): Sequence<Repository> = getResponses(criteria(BASE_URL))
-        .asSequence()
-        .flatMap { it.getJSONArray(VALUES_FIELD) }
-        .map { BitbucketRepository(JSONObject("$it")) }
-
-    private fun getResponses(url: String): Set<JSONObject> {
-        val responses = mutableSetOf<JSONObject>()
-        responses.add(doGETRequest(url))
-        check(!responses.last().hasError()) {
-            responses.last().getJSONObject(ERROR_FIELD).get(MESSAGE_FIELD).toString()
-        }
-        if (responses.last().hasNext()) {
-            responses.addAll(getResponses(responses.last().get(NEXT_PAGE_FIELD).toString()))
-        }
-        return responses
-    }
-
-    private tailrec fun doGETRequest(url: String): JSONObject {
-        val request = Unirest.get(url).header(ACCEPT_HEADER_FIELD, ACCEPT_HEADER_VALUE)
-        encodedAuthenticationToken?.let { request.header(AUTH_HEADER_FIELD, "$AUTH_HEADER_PREFIX $it") }
-        val response = request.asBinary()
-        check(response.status != UNAUTHORIZED_CODE && response.status != FORBIDDEN_CODE) {
-            "HTTP Response: ${response.statusText}. ${IOUtils.toString(response.body, StandardCharsets.UTF_8)}"
-        }
-        val shouldBeJSON = IOUtils.toString(response.body, StandardCharsets.UTF_8)
-        val parsedResponse = runCatching { JSONObject(shouldBeJSON) }
-        if (parsedResponse.isSuccess) {
-            return parsedResponse.getOrThrow()
-        }
-        println("Error parsing response from Bitbucket: ${parsedResponse.exceptionOrNull()?.message}")
-        println("Received non-JSON payload: $shouldBeJSON")
-        println("Assuming a server-side error, throttling requests for 60 seconds.")
-        Thread.sleep(WAIT_TIME)
-        return doGETRequest(url)
-    }
-
-    private fun JSONObject.hasError(): Boolean = !isNull(ERROR_FIELD)
-
-    private fun JSONObject.hasNext(): Boolean = !isNull(NEXT_PAGE_FIELD)
+    override fun byCriteria(criteria: BitbucketSearchCriteria): Sequence<Repository> =
+        responses[criteria(BASE_URL) to encodedAuthenticationToken]
+            .asSequence()
+            .flatMap { it.getJSONArray(VALUES_FIELD) }
+            .map { BitbucketRepository(JSONObject("$it")) }
 }
